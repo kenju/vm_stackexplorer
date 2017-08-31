9649f967e70776e5dba12654f4efad844adf098f
Kenju Wagatsuma <ken901waga@gmail.com>
2017-08-31 17:37:30 +0900

    implement stack number

--- explorer/src/Frame.js
+++ explorer/src/Frame.js
@@ -4,19 +4,23 @@ import './Frame.css';
 
 class Frame extends Component {
   renderStacks(stacks) {
-    return Array.from(stacks).map((stack) => {
+    return Array.from(stacks).map((stack, index) => {
       return (
-        <ul className="Stack-list">
-          <li className="Stack-listItem">{stack.count}</li>
-          <li className="Stack-listItem">{stack.pc}</li>
-          <li className="Stack-listItem">{stack.sp}</li>
-          <li className="Stack-listItem">{stack.ep}</li>
-          <li className="Stack-listItem">{stack.type}</li>
-          <li className="Stack-listItem">{stack.insns}</li>
-        </ul>
+        <div className="Stack-row">
+          <div className="Stack-num">
+            {stacks.length - index}
+          </div>
+          <ul className="Stack-list">
+            <li className="Stack-listItem">c: {stack.count}</li>
+            <li className="Stack-listItem">pc: {stack.pc}</li>
+            <li className="Stack-listItem">sp: {stack.sp}</li>
+            <li className="Stack-listItem">ep: {stack.ep}</li>
+            <li className="Stack-listItem">{stack.type}</li>
+            <li className="Stack-listItem">{stack.insns}</li>
+          </ul>
+        </div>
       );
     });
-
   }
 
   renderFrames() {
